875ff29d9ac17da47e95e7ab7bbd550cf4adf554
s0546567 <s0546567@htw-berlin.de>
2014-04-18 03:15:55 +0200

    adde testausgabe.java zur repo

---
+++ testausgabe.java
@@ -0,0 +1,17 @@
+
+public class testausgabe {
+
+	public static void main(String[] args) {
+		zaehl();
+	}
+
+	
+	public static void zaehl(){
+		int zahl = 0;
+		for(int i = 0;i<100;i++){
+			zahl++;
+			System.out.println(zahl);
+		}
+		
+	}
+}
